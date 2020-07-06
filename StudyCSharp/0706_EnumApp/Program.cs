ff4a153195c3d12f9e08c62c96ff75712b2ceac5
seungjae <nimbus112@naver.com>
2020-07-06 09:50:20 +0900

    200706 StudyCSharp start

---
+++ StudyCSharp/0706_EnumApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0706_EnumApp
+{
+    class Program
+    {
+        enum DialogResult
+        {
+            YES = 10,
+            NO = 20,
+            CANCEL,
+            CONFIRM,
+            OK
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(DialogResult.OK);
+            //Console.WriteLine((int)DialogResult.OK);
+            DialogResult result = DialogResult.YES;
+            if (result == DialogResult.YES)
+            {
+                Console.WriteLine("YES를 선택했습니다.");
+            }
+
+        }
+    }
+}
